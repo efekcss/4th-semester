fc076fd2ca36ffd8576f65c26841fa5540f6e5ee
efekcss <kocasuhasanefe@gmail.com>
2026-03-23 23:45:43 +0300

    VSCode üzerinde jupyter düzgün çalışmadı kernel ve çekirdek tam kendini yükleyemedi uyarısı da verdi bu yüzden güncellenmiyormuş, jupyter lab üzerinden jupyter kullanacağım ve ödevin teknik kısmını buradan halledeceğim.

---
+++ İST266-1 Hipotez Testleri/proje_ödevi/.ipynb_checkpoints/direct-checkpoint.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd #Daha sonra kullanım için
+import matplotlib as mpl # Daha sonra kullanma için
+import os
+
+"""
+Ödev 1
+"""
+if not os.path.exists('odev1_kitle.npy'):
+    degerler = np.random.normal(loc=50, scale=10, size=10000)
+    np.save('odev1_kitle.npy', degerler)
+
+kitle = np.load('odev1_kitle.npy')
+
+print(kitle)
+
+"""
+Bölüm 1 - Kitle Parametrelerinin Hesaplanması
+ - Kitle oratalaması 
+ - Kitle varyansı 
+hesabı
+"""
+# Kitle ortalaması
+toplam = 0
+for i in kitle:
+    toplam += i
+kitle_ort = toplam/len(kitle)
+print(f"Kitle Ortalaması = {kitle_ort}")
+
+# Kitle varyansı
+
+toplam = 0
+for i in kitle:
+    denklem = (i - kitle_ort)**2
+    toplam += denklem
+kitle_var = toplam/len(kitle)
+print(f"Kitle Varyansı = {kitle_var}")
